316661fa4afa3d49fe539c09caded1619f59e6f4
nd419 <5161147+neeldug@users.noreply.github.com>
2021-03-12 15:48:15 +0000

    add 1-pass ewma filter and fix formatting

--- calculations.py
+++ calculations.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+
+
 class Accleration:
     a = []
     alpha = []
@@ -10,27 +12,35 @@ class Accleration:
     def __init__(self):
         f = open("lowPass.txt", "r")
         data = f.readlines()
-        data[0] = re.split('\t+|\s+',data[0])
+        data[0] = re.split('\t+|\s+', data[0])
         self.alpha = [float(x) for x in data[0]]
         num = len(data[0])
         self.a = [0 for i in range(num)]
 
-    def append(self,accleration):
-        self.a.insert(0,accleration)
+    def append(self, accleration):
+        self.a.insert(0, accleration)
         self.a = self.a[:-1]
         return
 
     def fir(self):
         total = 0
-        for x in [self.a[i]*self.alpha[i] for i in range(11)]:
+        for x in [self.a[i] * self.alpha[i] for i in range(11)]:
             total += x
         self.fir_data = total
 
-    def userA(self):
+    def user_a(self):
         return self.a[0] - self.fir_data
 
-    def gravitaionalA(self):
+    def gravitational_a(self):
         return self.fir_data
+
+
+def fir(a, alpha):
+    total = 0
+    for x in [a[i] * alpha[i] for i in range(len(alpha))]:
+        total += x
+    return total
+
 
 class Accelerometer:
     x = Accleration()
@@ -42,56 +52,49 @@ class Accelerometer:
     def __init__(self):
         f = open("bandPass.txt", "r")
         data = f.readlines()
-        data[0] = re.split('\t+|\s+',data[0])
+        data[0] = re.split('\t+|\s+', data[0])
         self.alpha = [float(x) for x in data[0]]
         num = len(data[0])
         self.a = [0 for i in range(num)]
 
-
-    def append(self,accleration):
+    def append(self, accleration):
         self.x.append(accleration[0])
         self.y.append(accleration[1])
         self.z.append(accleration[2])
         return
-    
-    def userAccelerationGravitationalDirection(self):
+
+    def user_acceleration_gravitational_direction(self):
         self.x.fir()
         self.y.fir()
         self.z.fir()
-        x_sum = self.x.userA() * self.x.gravitaionalA()
-        y_sum = self.y.userA() * self.y.gravitaionalA()
-        z_sum = self.z.userA() * self.z.gravitaionalA()
+        x_sum = self.x.user_a() * self.x.gravitational_a()
+        y_sum = self.y.user_a() * self.y.gravitational_a()
+        z_sum = self.z.user_a() * self.z.gravitational_a()
         return x_sum + y_sum + z_sum
 
-    def fir(self,a,alpha):
-        total = 0
-        for x in [a[i]*alpha[i] for i in range(len(alpha))]:
-            total += x
-        return total
-
     def filter(self):
-        self.a.insert(0,(self.userAccelerationGravitationalDirection()))
+        self.a.insert(0, (self.user_acceleration_gravitational_direction()))
         self.a = self.a[:-1]
-        result = self.fir(self.a,self.alpha)
+        result = fir(self.a, self.alpha)
         return result
-        
+
 
 class StepDetection:
     a = Accelerometer()
     step_data = []
 
-    def __init__(self,data):
-        self.data = pd.read_csv(data)#,sep="\t")
-        #self.data.to_csv("data4.csv")
+    def __init__(self, data):
+        self.data = pd.read_csv(data)  # ,sep="\t")
+        # self.data.to_csv("data4.csv")
 
     def run_filter(self):
         for i in range(len(self.data)):
-            self.a.append([self.data["x_data"][i],self.data["y_data"][i],self.data["z_data"][i]])
+            self.a.append([self.data["x_data"][i], self.data["y_data"][i], self.data["z_data"][i]])
             self.step_data.append(self.a.filter())
-    
+
     def plot_data(self):
         time_data = range(len(self.step_data))
-        plt.plot(time_data,self.step_data)
+        plt.plot(time_data, self.step_data)
         plt.show()
 
     def smoothdata(self):
@@ -99,20 +102,26 @@ class StepDetection:
         filter_constant = 30
         filtered = []
         for x in self.step_data:
-            filt = (x+filterprev*filter_constant)/(filter_constant+1)
+            filt = (x + filterprev * filter_constant) / (filter_constant + 1)
             filtered.append(filt)
             filterprev = filt
         self.step_data = filtered
 
+    def ewma(self, window_size):
+        a = 2 / (window_size + 1.0)
+        for i in range(1, len(self.step_data)):
+            self.step_data[i] = a * self.step_data[i] + (1 - a) * self.step_data[i - 1]
+        print(self.step_data)
+        return
+
     def averagedata(self):
         window = 10
         filtered = []
-        for i in range(len(self.step_data)-window):
-            filt = np.mean(self.step_data[i:i+10])
+        for i in range(len(self.step_data) - window):
+            filt = np.mean(self.step_data[i:i + 10])
             filtered.append(filt)
         self.step_data = filtered
-
-    
+        return
 
     def count_peaks(self):
         # count = 0
@@ -133,7 +142,7 @@ class StepDetection:
         #         if goingup == False:
         #             goingup = True
         #             last_min = prevx
-        #     prevx = x       
+        #     prevx = x
 
         count = 0
         below = True
@@ -152,7 +161,8 @@ class StepDetection:
 
 step = StepDetection('data3.csv')
 step.run_filter()
-step.averagedata()
-#step.smoothdata()
+# step.averagedata()
+# step.smoothdata()
+# step.ewma(1)
 step.count_peaks()
 step.plot_data()
